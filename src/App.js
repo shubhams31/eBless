ca640dd852466e6aa65cbc2bb8e0ed61e62db0bc
s-srivastava <s.srivastava@salesforce.com>
2025-06-27 16:02:37 +0530

    Fix GitHub Pages routing by switching to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import styled from 'styled-components';
 import Header from './components/Header';
 import RecordingPage from './components/RecordingPage';
